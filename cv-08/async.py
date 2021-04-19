db573dab001a565d3dc4e7eb6d9eb449377b66ec
JaroslavVilhan <jvilhan94@gmail.com>
2021-04-19 23:04:22 +0200

    feat(cv-08): finishing asynchrouns version

--- cv-08/async.py
+++ cv-08/async.py
@@ -1,3 +1,5 @@
+import time
+import asyncio
 import aiohttp
 import aiofiles
 
@@ -8,3 +10,21 @@ async def download_file(url, filename):
             data = await resp.read()
     async with aiofiles.open(filename, "w+b") as outfile:
         await outfile.write(data)
+
+
+async def main():
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        download_file('https://uim.fei.stuba.sk/wp-content/uploads/2018/02'
+                      '/2021-07.iteracia-generator-korutina.pdf', 'file0.pdf'),
+        download_file('https://uim.fei.stuba.sk/wp-content/uploads/2018/02'
+                      '/2021-01.uvod-do-paralelnych-a-distribuovanych'
+                      '-vypoctov.pdf', 'file1.pdf'),
+        download_file('https://uim.fei.stuba.sk/wp-content/uploads/2018/02'
+                      '/2021-04b.diningphilosophers.pdf', 'file2.pdf')
+    )
+    elapsed = time.perf_counter() - start_time
+    print(f"\nTotal elapsed time: {elapsed:.1f} sec")
+
+if __name__ == "__main__":
+    asyncio.run(main())
